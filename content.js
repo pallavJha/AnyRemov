28822b578313f5dbfa244fc53a8eec948b9782fb
Pallav Jha <pallav.jha@soroco.com>
2018-08-26 19:55:25 +0530

    added code for chrome extension

--- content.js
+++ content.js
@@ -1,6 +1,66 @@
 window.onload = function() {
     console.log("page load!");
-    chrome.storage.sync.get('color', function(data) {
+    chrome.storage.local.get('color', function(data) {
         console.log(data);
     });
+
+    console.log("Hostname = " + window.location.hostname)
+
+    chrome.storage.local.get(
+        ["!!!AnyRemove!!!"],
+        function(items) {
+            let allCollections;
+            if (!items || (Object.keys(items).length === 0 && items.constructor === Object)) {
+                //Adding data for the first time
+                console.log("No Values were found!")
+                allCollections = {}
+            } else {
+                //data is already present which means that the app is being used
+                allCollections = items["!!!AnyRemove!!!"];
+                allCollections = JSON.parse(allCollections)
+                thisHostSelector = allCollections[window.location.hostname];
+                removeElements(window.location.hostname, thisHostSelector);
+                commonSelectorsForAllHosts = allCollections["*"];
+                removeElements("*", commonSelectorsForAllHosts);
+            }
+        });
+}
+
+function removeElements(hostname, selectors) {
+    if (selectors && selectors.length > 0) {
+        splited_selectors = selectors.split(";");
+        for (var i = 0; i < splited_selectors.length; i++) {
+            try {
+                selector = splited_selectors[i];
+                selector = selector.trim();
+                if (!selector || selector.length == 0) {
+                	continue;
+                }
+                console.log(selector);
+                removeElementsFromSelector(selector);
+            } catch (e) {
+                console.error("Error while executing the removal process with hostname = " + hostname +
+                    " splited_selectors = " + splited_selectors + " i = " + i, e);
+            }
+        }
+    }
+}
+
+function removeElementsFromSelector(selector) {
+    elements = Array.from(document.querySelectorAll(selector));
+    if (elements) {
+        console.log("Found " + elements.length + " elements for removal process with selector = " + selector);
+        for (var i = 0; i < elements.length; i++) {
+            try {
+                let element = elements[i];
+                if (element) {
+                    element.parentNode.removeChild(element);
+                }
+            } catch (e) {
+                console.error("Error while removing element", e);
+            }
+        }
+    } else {
+        console.log("No elements were found with selector = " + selector)
+    }
 }
--- popup.js
+++ popup.js
@@ -23,21 +23,22 @@
       return /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$|^(([a-zA-Z]|[a-zA-Z][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z]|[A-Za-z][A-Za-z0-9\-]*[A-Za-z0-9])$|^\s*((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:)))(%.+)?\s*$/.test(str);
   }
 
-
   let saveButton = document.getElementById('saveButton');
+
+
 
   saveButton.onclick = function(element) {
 
       console.log("ABC");
-      var host = document.getElementById("host").value;
-      var selectors = document.getElementById("selectors").value;
+      let host = document.getElementById("host").value;
+      let selectors = document.getElementById("selectors").value;
 
 
       if (host === "*" || validateHost(host)) {
           chrome.storage.local.get(
               ["!!!AnyRemove!!!"],
               function(items) {
-                  var allCollections;
+                  let allCollections;
                   if (!items || (Object.keys(items).length === 0 && items.constructor === Object)) {
                       //Adding data for the first time
                       allCollections = "{}";
@@ -49,7 +50,7 @@
 
                   allCollections = JSON.parse(allCollections)
                   allCollections[host] = selectors;
-                  allCollectionsStr = JSON.stringify(allCollections);
+                  let allCollectionsStr = JSON.stringify(allCollections);
 
                   chrome.storage.local.set({
                       "!!!AnyRemove!!!": allCollectionsStr
@@ -58,74 +59,209 @@
                   });
 
                   console.log("Updated the selectors for " + host);
-                  document.getElementById("errors").style.visibility = "hidden";
+                  makeErrorsInVisible();
+                  clearTable();
               });
       } else {
-          if (document.getElementById("errors").style.visibility == "hidden") {
-              //its visible
-              //check if input fie type has file selected
-              document.getElementById("errors").style.visibility = "visible"
-          }
-          console.error("Invalid hostname inserted, Please try again with valid hostname");
+          makeErrorsVisible();
+          console.error("Invalid hostname inserted, Please try again with valid hostname.");
       }
   };
 
 
+  function makeErrorsVisible() {
+      if (document.getElementById("errors").style.display == "none") {
+          //its visible
+          //check if input fie type has file selected
+          document.getElementById("errors").style.display = "block"
+      }
+  }
+
+  function makeErrorsInVisible() {
+      document.getElementById("errors").style.display = "none";
+  }
+
 
   let showAllButton = document.getElementById('showAllButton');
 
-  showAllButton.onclick = function(element) {
+  showAllButton.onclick = addTable;
 
-      chrome.storage.local.get({
-          OUR_KEY_CONSTANT_FOR_LOCAL_STORAGE
-      }, function(items) {
-          if (!items) {
-              //Adding data for the first time
-              console.log("No Values were found!")
-              allCollections = {}
-          } else {
-              //data is already present which means that the app is being used
-              allCollections = JSON.parse(items);
-          }
+  function addTable(element) {
+
+      makeErrorsInVisible();
+      chrome.storage.local.get(
+          ["!!!AnyRemove!!!"],
+          function(items) {
+              let allCollections;
+              if (!items || (Object.keys(items).length === 0 && items.constructor === Object)) {
+                  //Adding data for the first time
+                  console.log("No Values were found!")
+                  allCollections = {}
+              } else {
+                  //data is already present which means that the app is being used
+                  allCollections = items["!!!AnyRemove!!!"];
+              }
+
+              allCollections = JSON.parse(allCollections)
+
+              let table = document.getElementById("allCollections");
+              if (table) {
+                  table.parentNode.removeChild(table);
+              }
+
+              table = document.createElement("table")
+              table.setAttribute("id", "allCollections");
+
+              let thead = document.createElement("thead")
+
+              let th1 = document.createElement("th")
+              th1.appendChild(document.createTextNode("Host"));
+
+              let th2 = document.createElement("th")
+              th2.appendChild(document.createTextNode("Identifier(';' Separated)"));
+
+              let th3 = document.createElement("th")
+              th3.appendChild(document.createTextNode("Actions"));
+
+              thead.appendChild(th1)
+              thead.appendChild(th2)
+              thead.appendChild(th3)
+              table.appendChild(thead)
+
+              let tbody = document.createElement("tbody")
+
+              for (let host in allCollections) {
+                  let tr = document.createElement("tr");
+
+                  let host_td = document.createElement("td")
+                  host_td.classList.add("allCollections");
+                  let _host = document.createTextNode(host);
+                  host_td.appendChild(_host);
+
+                  let selectors_td = document.createElement("td");
+                  selectors_td.classList.add("allCollections");
+                  let selectors = document.createTextNode(allCollections[host]);
+                  selectors_td.appendChild(selectors);
+
+                  let actions_td = document.createElement("td");
+                  actions_td.classList.add("allCollections");
+
+                  let editButton = document.createElement("button");
+                  editButton.appendChild(document.createTextNode("Edit"));
+                  editButton.classList.add("edit_button");
+                  editButton.classList.add("button");
+                  editButton.onclick = function() {
+                      editHost(host);
+                  }
+
+                  let delButton = document.createElement("button");
+                  delButton.appendChild(document.createTextNode("Delete"));
+                  delButton.classList.add("delete_button");
+                  delButton.classList.add("button");
+                  delButton.onclick = function() {
+                      deleteHost(host);
+                  }
+
+                  actions_td.appendChild(editButton);
+                  actions_td.appendChild(delButton);
+
+                  tr.appendChild(host_td);
+                  tr.appendChild(selectors_td);
+                  tr.appendChild(actions_td);
+                  tbody.appendChild(tr);
+              }
+              table.appendChild(tbody);
+
+              document.getElementById("body").appendChild(table);
+          });
+  }
 
 
+  let clearAllButton = document.getElementById('clearAllButton');
 
-          var table = document.getElementById("allCollections");
-          if (table) {
-              table.parentNode.removeChild(table);
-          }
+  clearAllButton.onclick = clearTable;
 
-          table = document.createElement("table")
-          thead = document.createElement("thead")
+  function clearTable() {
+      let table = document.getElementById("allCollections");
+      if (table) {
+          table.parentNode.removeChild(table);
+      }
+      return table;
+  }
 
-          th1 = document.createElement("th")
-          th1.appendChild(document.createTextNode("Host"));
+  function deleteHost(host) {
 
-          th2 = document.createElement("th")
-          th2.appendChild(document.createTextNode("Identifier(';' Separated)"));
+      console.log("Deleting host = " + host);
 
-          thead.appendChild(th1)
-          thead.appendChild(th2)
-          table.appendChild(thead)
+      if (host === "*" || validateHost(host)) {
+          chrome.storage.local.get(
+              ["!!!AnyRemove!!!"],
+              function(items) {
+                  let allCollections;
+                  if (!items || (Object.keys(items).length === 0 && items.constructor === Object)) {
+                      //Adding data for the first time
+                      allCollections = "{}";
 
-          tbody = document.createElement("tbody")
+                  } else {
+                      //data is already present which means that the app is being used
+                      allCollections = items["!!!AnyRemove!!!"];
+                  }
 
-          for (var host in allCollections) {
-              var tr = document.createElement("tr");
+                  allCollections = JSON.parse(allCollections)
+                  delete allCollections[host];
+                  allCollectionsStr = JSON.stringify(allCollections);
 
-              var host_td = document.createElement("td")
-              var _host = document.createTextNode(host);
-              host_td.appendChild(_host);
+                  chrome.storage.local.set({
+                      "!!!AnyRemove!!!": allCollectionsStr
+                  }, function() {
+                      console.log("Data deleted for " + host + "!");
+                      addTable();
+                  });
 
-              var selectors_td = document.createElement("td");
-              var selectors = document.createTextNode(allCollections[host]);
-              selectors_td.appendChild(selectors);
 
-              tr.appendChild(td);
-              tbody.appendChild(tr);
-          }
-          table.appendChild(tbody);
+              });
+      }
+  }
 
-          document.getElementById("body").appendChild(table);
+  function editHost(host) {
+
+      console.log("Editing host = " + host);
+
+      if (host === "*" || validateHost(host)) {
+          chrome.storage.local.get(
+              ["!!!AnyRemove!!!"],
+              function(items) {
+                  let allCollections;
+                  if (!items || (Object.keys(items).length === 0 && items.constructor === Object)) {
+                      //Adding data for the first time
+                      allCollections = "{}";
+
+                  } else {
+                      //data is already present which means that the app is being used
+                      allCollections = items["!!!AnyRemove!!!"];
+                  }
+
+                  allCollections = JSON.parse(allCollections)
+                  let selectors = allCollections[host];
+
+                  document.getElementById("host").value = host;
+                  document.getElementById("selectors").value = selectors;
+              });
+      }
+  }
+
+  let fetchHostNameButton = document.getElementById('fetchHostNameButton');
+
+  fetchHostNameButton.onclick = getCurrentTabHostName;
+
+  function getCurrentTabHostName() {
+      chrome.tabs.query({
+          currentWindow: true,
+          active: true
+      }, function(tabs) {
+          console.log(tabs[0].url);
+          var url = new URL(tabs[0].url)
+          var domain = url.hostname
+          document.getElementById("host").value = domain;
       });
-  };
+  }
